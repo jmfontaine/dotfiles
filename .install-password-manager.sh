d507fa581f72e485010db33b1bf38d1c18759485
Jean-Marc Fontaine <jm@jmfontaine.net>
2024-10-27 17:45:09 -0400

    Switch to chezmoi

---
+++ .install-password-manager.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Exit immediately if op is already in $PATH
+type op >/dev/null 2>&1 && exit
+
+brew install 1password 1password-cli
+open https://support.1password.com/secret-key/#find-your-secret-key-in-the-1password-apps
+open https://developer.1password.com/docs/cli/app-integration
