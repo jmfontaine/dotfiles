4a888ea85941fa4bde7a0af8c7f0bf0bd1bdddad
Jean-Marc Fontaine <jm@jmfontaine.net>
2024-11-28 12:11:43 -0500

    Change file structure

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install 1Password and chezmoi
 brew install 1password 1password-cli chezmoi
-open https://support.1password.com/secret-key/#find-your-secret-key-in-the-1password-apps
-open https://developer.1password.com/docs/cli/app-integration
 
 # Log into 1Password
 eval $(op signin)
