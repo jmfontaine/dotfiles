2c4d4cda3e04c5450c5759370ba4ab07bdafc684
Jean-Marc Fontaine <jm@jmfontaine.net>
2025-02-22 15:33:28 -0500

    Improve installation proicess and instructions

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -eufo pipefail
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
+echo ""
+echo "🤚  This script will set up your local machine."
+read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
 
-# Install 1Password and chezmoi
-brew install 1password 1password-cli chezmoi
+if command -v brew &>/dev/null; then
+  echo "🍺  Homebrew already installed. Skipping installation."
+else
+  echo "🍺  Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
-# Log into 1Password
-eval $(op signin)
+if command -v chezmoi &>/dev/null; then
+  echo "🏠  chezmoi already installed. Skipping installation."
+else
+  echo "🏠  Installing chezmoi"
+  brew install chezmoi
+fi
 
-# Setup local machine
-exec chezmoi init --apply --keep-going jmfontaine
+if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
+  echo "🚀  chezmoi already initialized. Skipping initialization."
+else
+  echo "🚀  Bootstrapping machine"
+  exec chezmoi init --apply --keep-going jmfontaine
+fi
